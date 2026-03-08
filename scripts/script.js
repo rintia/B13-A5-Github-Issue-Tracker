67a88d05831ab50715d57842d0b4ceb17e7ec8f5
Rintia Islam <rintiaislam110@gmail.com>
2026-03-08 11:43:26 +0600

    search funtionality added

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,42 @@ const loadIssue = () =>{
     .then(data => displayIssues(data.data))
 }
 
+const loadSingleIssue = async(id)=>{
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res = await fetch(url)
+    const json = await res.json()
+    const issue = json.data;
+
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML =`
+              <h2 class="text-2xl font-bold">${issue.title}</h2>
+                    <div class="flex gap-4 text-sm">
+                        <div class="badge badge-success mr-4">${issue.status}</div>
+                        <ul class="list-disc flex gap-6">
+                            <li>Opened by ${issue.assignee}</li>
+                            <li>${new Date(issue.updatedAt).toISOString().split('T')[0]}</li>
+                        </ul>
+                    </div>
+                    ${createBadges(issue.labels)}
+                    <p>${issue.description}</p>
+                    <div class="bg-base-200 p-4 flex justify-between">
+                        <div>
+                            <h4 class="text-gray-700">Asignee:</h4>
+                            <p class="font-semibold">${issue.assignee}</p>
+                        </div>
+                        <div>
+                            <h4 class="text-gray-700">Priority:</h4>
+                            <div class="badge border-yellow-500 badge-soft badge-warning">${issue.priority}</div>
+                        </div>
+                    </div>
+    `
+
+   document.getElementById('issue_details').showModal()
+
+}
+
+
+
 const createBadges = (arr) =>{
     const badge = arr.map(el => `<div class="badge border-yellow-500 badge-soft badge-warning">${el}</div>`);
     return badge.join(' ')
@@ -17,7 +53,7 @@ const displayIssues =(issues)=>{
     issues.forEach(issue => {
         const issueCard = document.createElement('div');
         issueCard.innerHTML =`
-        <div class="py-4 rounded-lg space-y-2 h-full shadow-sm bg-base-100">
+        <div onclick="loadSingleIssue(${issue.id})" class="py-4 hover:cursor-pointer rounded-lg space-y-2 h-full shadow-sm bg-base-100">
         <div class='flex justify-between px-4'>
         <div>${issue.status == 'open' ? `<img src='./assets/Open-Status.png'/>` : `<img src="./assets/Closed- Status .png" alt="">` }</div>
         
@@ -41,4 +77,11 @@ const displayIssues =(issues)=>{
     });
 }
 
-loadIssue()
+loadIssue();
+
+const searchIssues =()=>{
+    const searchInput = document.getElementById('search-issue').value;
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchInput}`)
+    .then(res=> res.json())
+    .then(search => displayIssues(search.data))
+}
