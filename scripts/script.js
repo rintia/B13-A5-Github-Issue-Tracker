c8aedf151c2b615194b3e179c649ec3084d578ec
Rintia Islam <rintiaislam110@gmail.com>
2026-03-08 10:48:49 +0600

    modal designed

--- scripts/script.js
+++ scripts/script.js
@@ -18,8 +18,9 @@ const displayIssues =(issues)=>{
         const issueCard = document.createElement('div');
         issueCard.innerHTML =`
         <div class="py-4 rounded-lg space-y-2 h-full shadow-sm bg-base-100">
-        <div class='flex justify-between'>
-        <img src='./assets/Open-Status'/>
+        <div class='flex justify-between px-4'>
+        <div>${issue.status == 'open' ? `<img src='./assets/Open-Status.png'/>` : `<img src="./assets/Closed- Status .png" alt="">` }</div>
+        
         <div class="badge border-yellow-500 badge-soft badge-warning">${issue.priority}</div>
         </div>
         <h2 class="px-4 text-2xl font-semibold">${issue.title}</h2>
